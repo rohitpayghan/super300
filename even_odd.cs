7426a218492e85b2a8cf4f60cb9a6aac206ffd5c
Rohit Payghan <156587897+rohitpayghan@users.noreply.github.com>
2026-01-28 10:38:43 +0530

    Add files via upload

---
+++ even_odd.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+            Console.WriteLine("Number is Even");
+        else
+            Console.WriteLine("Number is Odd");
+    }
+}
