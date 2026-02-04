ce93586c7dcd596a810c779ae76a377a4606ea4a
Rohit Payghan <156587897+rohitpayghan@users.noreply.github.com>
2026-02-04 11:45:50 +0530

    Add files via upload

---
+++ app_1/Add/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int sum = a + b;
+        Console.WriteLine("Sum = " + sum);
+    }
+}
